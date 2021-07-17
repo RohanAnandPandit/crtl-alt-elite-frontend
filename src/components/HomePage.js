a6c9d279a36dcb184339ff92306d65ba11c2ecd7
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-17 15:48:05 +0100

    Home page with links for some websites

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,84 +1,40 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import AppMenu from "./AppMenu";
+import Link from '@material-ui/core/Link';
+import "linkedin.svg";
+import "github.svg";
+import "instagram.svg";
+import "./HomePage.css";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-function SimpleMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
-}
-
-function AppMenu() {
-    const classes = useStyles();
-
-    return (
-    <div className={classes.root}>
-        <SimpleMenu/>
-    
-        <AppBar position="static">
-        <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-            ProTip
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar>
-        </AppBar>
-    </div>
-    );
-}
-
+const websites = [
+    'linkedin',
+    'github',
+]
 export default class HomePage extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <AppMenu/>
+                <div className="logo-container">
+                    {websites.map((site) => (
+                        <div>
+                            <Link href={`/tips/${site}`}>
+                                <div className="logo">
+                                    <img src={`${site}.svg`} alt={`${site}`}></img>
+                                </div>
+                            </Link>
+                        </div>
+                    ))
+                    }
+
+                <div>
+                    <Link href={`/tips/instagram`}>
+                        <div className="logo">
+                            <img src={`instagram.svg`} alt={`instagram`} width="100px" height="100px"></img>
+                        </div>
+                    </Link>
+                </div>
+                </div>
             </React.Fragment>
         );
     }
