32285b0167298c94b8426d924b2839102d9e5263
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-17 14:11:00 +0100

    Create component for HomePage with menu bar

---
+++ src/components/HomePage.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+function AppMenu() {
+    const classes = useStyles();
+
+    return (
+    <div className={classes.root}>
+        <SimpleMenu/>
+    
+        <AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+            ProTip
+            </Typography>
+            <Button color="inherit">Login</Button>
+        </Toolbar>
+        </AppBar>
+    </div>
+    );
+}
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <AppMenu/>
+            </React.Fragment>
+        );
+    }
+}
