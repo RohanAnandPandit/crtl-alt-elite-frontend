850497251fe1754ec6cadf806643bff96febccb3
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-07-17 23:26:55 +0100

    Display checkboxes for each tip

--- src/components/TipsPage.js
+++ src/components/TipsPage.js
@@ -1,5 +1,56 @@
 import React from "react";
 import AppMenu from "./AppMenu";
+import "./TipsPage.css";
+import { makeStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const websites = require('./websites.json');
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+}));
+
+function CheckboxesGroup(props) {
+  const classes = useStyles();
+  const checked = {}
+  console.log('website', props);
+  const tips = websites[props.website];
+  tips.forEach((tip) => {
+      checked[tip] = false;
+  })
+  const [state, setState] = React.useState(checked);
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Tips</FormLabel>
+        <FormGroup>
+            {tips.map((tip, index) => (
+                <FormControlLabel
+                control={<Checkbox checked={state[index]} onChange={handleChange} name={tip} />}
+                label={tip}
+                />
+            ))}
+        </FormGroup>
+        <FormHelperText>Select ones you have done</FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
 
 export default class TipsPage extends React.Component {
     constructor() {
@@ -16,10 +67,14 @@ export default class TipsPage extends React.Component {
     }
 
     render() {
+        if (this.state.website === '') {
+            return "Loading...";
+        }
         return (
             <React.Fragment>
                 <AppMenu/>
-                {this.state.website}
+                <h1>{this.state.website}</h1>
+                <CheckboxesGroup website={this.state.website}/>
             </React.Fragment>
         );
     }
